b94347297bce4a3f9946e321299e08221af42db8
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-27 21:59:43 +0100

    feat(presentation): create gallery screen (wip)

---
+++ composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/gallery/presentation/MemeGalleryScreen.kt
@@ -0,0 +1,13 @@
+package com.example.memecreatorappproject.gallery.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.memecreatorappproject.core.presentation.MemeTemplate
+
+@Composable
+fun MemeGalleryScreen(
+    modifier: Modifier = Modifier,
+    onClicked: (MemeTemplate) -> Unit,
+) {
+    // TODO: provide impl
+}
