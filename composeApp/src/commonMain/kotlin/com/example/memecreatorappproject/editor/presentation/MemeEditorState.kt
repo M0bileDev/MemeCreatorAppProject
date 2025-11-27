b3948007ba6413509abd612e29053560d36a1d69
m0bileDev <damian.ogorek.1996@gmail.com>
2025-11-27 23:48:00 +0100

    feat(strings): introduce state of meme editor

---
+++ composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/MemeEditorState.kt
@@ -0,0 +1,24 @@
+package com.example.memecreatorappproject.editor.presentation
+
+import androidx.compose.ui.unit.IntSize
+
+typealias TextBoxId = String
+
+data class MemeEditorState(
+    val templateSize: IntSize = IntSize.Zero,
+    val abortWithoutSave: Boolean = false,
+    val textBoxInteractionState: TextBoxInteractionState = TextBoxInteractionState.None,
+    val memeTexts: List<MemeText> = emptyList(),
+)
+
+sealed interface TextBoxInteractionState {
+    data object None : TextBoxInteractionState
+
+    data class Selected(
+        val textBoxId: TextBoxId,
+    ) : TextBoxInteractionState
+
+    data class Editing(
+        val textBoxId: TextBoxId,
+    ) : TextBoxInteractionState
+}
