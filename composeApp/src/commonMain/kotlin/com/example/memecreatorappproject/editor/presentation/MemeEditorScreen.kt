9377a98f23ca44164313fdeb11a9c922fabfa15c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-12-04 22:23:11 +0100

    refactor(presentation): add components to view

--- composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/MemeEditorScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/MemeEditorScreen.kt
@@ -1,15 +1,32 @@
 package com.example.memecreatorappproject.editor.presentation
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.memecreatorappproject.core.presentation.MemeTemplate
+import com.example.memecreatorappproject.core.theme.MemeCreatorTheme
+import memecreatorappproject.composeapp.generated.resources.Res
+import memecreatorappproject.composeapp.generated.resources.meme_template_01
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun MemeEditorRoot(viewModel: MemeEditorViewModel = koinViewModel()) {
+fun MemeEditorRoot(
+    viewModel: MemeEditorViewModel = koinViewModel(),
+    memeTemplate: MemeTemplate,
+) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
     MemeEditorScreen(
+        memeTemplate = memeTemplate,
         state = state,
         onAction = viewModel::onAction,
     )
@@ -17,7 +34,33 @@ fun MemeEditorRoot(viewModel: MemeEditorViewModel = koinViewModel()) {
 
 @Composable
 fun MemeEditorScreen(
+    modifier: Modifier = Modifier,
+    memeTemplate: MemeTemplate,
     state: MemeEditorState,
     onAction: (MemeEditorAction) -> Unit,
 ) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth,
+            painter = painterResource(memeTemplate.drawable),
+            contentDescription = null,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMemeEditorScreen() {
+    MemeCreatorTheme {
+        MemeEditorScreen(
+            memeTemplate =
+                MemeTemplate(
+                    id = "meme_template_01",
+                    drawable = Res.drawable.meme_template_01,
+                ),
+            state = MemeEditorState(),
+            onAction = {},
+        )
+    }
 }
