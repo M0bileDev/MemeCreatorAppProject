93491672b3fd098ee93b977304fa9af0a52721f9
m0bileDev <damian.ogorek.1996@gmail.com>
2025-12-13 21:52:26 +0100

    refactor(presentation): update implementation
    Provide meme text box and pass the memeText data through loop

--- composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/MemeEditorScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/MemeEditorScreen.kt
@@ -9,9 +9,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.memecreatorappproject.core.presentation.MemeTemplate
 import com.example.memecreatorappproject.core.theme.MemeCreatorTheme
+import com.example.memecreatorappproject.editor.presentation.components.MemeTextBox
 import memecreatorappproject.composeapp.generated.resources.Res
 import memecreatorappproject.composeapp.generated.resources.meme_template_01
 import org.jetbrains.compose.resources.painterResource
@@ -39,13 +41,35 @@ fun MemeEditorScreen(
     state: MemeEditorState,
     onAction: (MemeEditorAction) -> Unit,
 ) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Image(
             modifier = Modifier.fillMaxWidth(),
             contentScale = ContentScale.FillWidth,
             painter = painterResource(memeTemplate.drawable),
             contentDescription = null,
         )
+        state.memeTexts.forEach { memeText ->
+            MemeTextBox(
+                memeText = memeText,
+                textBoxInteractionState = state.textBoxInteractionState,
+//                test purpose only
+                maxWidth = 500.dp,
+//                test purpose only
+                maxHeight = 500.dp,
+                onClick = {
+                    onAction(MemeEditorAction.OnSelectMemeText(textBoxId = memeText.id))
+                },
+                onDoubleClick = {
+                    onAction(MemeEditorAction.OnEditMemeText(textBoxId = memeText.id))
+                },
+                onTextChange = {
+                    onAction(MemeEditorAction.OnMemeTextChange(memeText.id, it))
+                },
+                onDeleteClick = {
+                    onAction(MemeEditorAction.OnDeleteMemeTextClick(textBoxId = memeText.id))
+                },
+            )
+        }
     }
 }
 
