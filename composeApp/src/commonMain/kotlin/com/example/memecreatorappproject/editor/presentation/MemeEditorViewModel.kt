93170c5fd7fbeb47358b927b1b577709ad9fee84
m0bileDev <damian.ogorek.1996@gmail.com>
2025-12-13 21:53:30 +0100

    refactor(presentation): provide implementation for actions

--- composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/MemeEditorViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/MemeEditorViewModel.kt
@@ -3,6 +3,7 @@ package com.example.memecreatorappproject.editor.presentation
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MemeEditorViewModel : ViewModel() {
     private val _state: MutableStateFlow<MemeEditorState> = MutableStateFlow(MemeEditorState())
@@ -13,15 +14,60 @@ class MemeEditorViewModel : ViewModel() {
             MemeEditorAction.OnAddTextClick -> TODO()
             MemeEditorAction.OnConfirmAbortWithoutSave -> TODO()
             is MemeEditorAction.OnContainerSizeChange -> TODO()
-            is MemeEditorAction.OnDeleteMemeTextClick -> TODO()
+            is MemeEditorAction.OnDeleteMemeTextClick -> deleteMemeText(action.textBoxId)
             MemeEditorAction.OnDismissAbortWithoutSave -> TODO()
-            is MemeEditorAction.OnEditMemeText -> TODO()
+            is MemeEditorAction.OnEditMemeText -> editMemeText(action.textBoxId)
             MemeEditorAction.OnGoBackClick -> TODO()
-            is MemeEditorAction.OnMemeTextChange -> TODO()
+            is MemeEditorAction.OnMemeTextChange -> updateMemeText(action.textBoxId, action.text)
             is MemeEditorAction.OnMemeTextTransformChange -> TODO()
             is MemeEditorAction.OnSaveMemeConfirm -> TODO()
-            is MemeEditorAction.OnSelectMemeText -> TODO()
+            is MemeEditorAction.OnSelectMemeText -> selectMemeText(action.textBoxId)
             MemeEditorAction.OnTapOutsideSelectedText -> TODO()
+        }
+    }
+
+    private fun deleteMemeText(textBoxId: TextBoxId) {
+        _state.update {
+            it.copy(
+                memeTexts =
+                    it.memeTexts.filter { memeText ->
+                        memeText.id != textBoxId
+                    },
+            )
+        }
+    }
+
+    private fun selectMemeText(textBoxId: TextBoxId) {
+        _state.update {
+            it.copy(
+                textBoxInteractionState = TextBoxInteractionState.Selected(textBoxId),
+            )
+        }
+    }
+
+    private fun updateMemeText(
+        textBoxId: TextBoxId,
+        text: String,
+    ) {
+        _state.update {
+            it.copy(
+                memeTexts =
+                    it.memeTexts.map { memeText ->
+                        if (memeText.id == textBoxId) {
+                            memeText.copy(text = text)
+                        } else {
+                            memeText
+                        }
+                    },
+            )
+        }
+    }
+
+    private fun editMemeText(textBoxId: TextBoxId) {
+        _state.update {
+            it.copy(
+                textBoxInteractionState = TextBoxInteractionState.Editing(textBoxId),
+            )
         }
     }
 }
