8811d0a2b48e8209a28581fb1aebeadc98023f6c
m0bileDev <damian.ogorek.1996@gmail.com>
2025-12-17 23:50:10 +0100

    refactor(presentation): update type from TextId to TextBoxId

--- composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/MemeText.kt
+++ composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/MemeText.kt
@@ -3,10 +3,8 @@ package com.example.memecreatorappproject.editor.presentation
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 
-typealias TextId = String
-
 data class MemeText(
-    val id: TextId,
+    val id: TextBoxId,
     val text: String,
     val fontSize: TextUnit = 36.sp,
     val offsetRatioX: Float = 0f,
