461a0a551dee0402b1dc4b7f2f705460a17122f8
m0bileDev <damian.ogorek.1996@gmail.com>
2025-12-17 23:18:50 +0100

    feature(components): introduce bottom bar

---
+++ composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/components/BottomBar.kt
@@ -0,0 +1,42 @@
+package com.example.memecreatorappproject.editor.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.memecreatorappproject.core.theme.MemeCreatorTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier,
+    onAddTextClick: () -> Unit,
+    onSaveClick: () -> Unit,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth().padding(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.End),
+    ) {
+        OutlinedButton(onClick = onAddTextClick) {
+            Text("Add text")
+        }
+        Button(onClick = onSaveClick) {
+            Text("Save meme")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewBottomBar() {
+    MemeCreatorTheme {
+        BottomBar(onAddTextClick = {}, onSaveClick = {})
+    }
+}
