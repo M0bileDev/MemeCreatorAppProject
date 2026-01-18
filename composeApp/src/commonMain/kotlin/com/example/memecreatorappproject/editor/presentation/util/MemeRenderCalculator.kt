affa0ff91cf1932792c3bd00c805703048e92024
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-18 01:38:27 +0100

    feat(util): provide helper function to scale meme text

--- composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/util/MemeRenderCalculator.kt
+++ composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/util/MemeRenderCalculator.kt
@@ -1,6 +1,9 @@
 package com.example.memecreatorappproject.editor.presentation.util
 
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.unit.IntSize
+import com.example.memecreatorappproject.editor.presentation.MemeText
+import kotlin.math.roundToInt
 
 class MemeRenderCalculator(
     private val displayDensity: Float,
@@ -28,6 +31,55 @@ class MemeRenderCalculator(
             scaleX = scaleX,
             scaleY = scaleY,
             bitmapScale = bitmapScale,
+        )
+    }
+
+    private fun calculateScaledMemeText(
+        memeText: MemeText,
+        scaleFactors: ScaleFactors,
+        templateSize: IntSize,
+    ): ScaledMemeText {
+        val (scaleX, scaleY, bitmapScale) = scaleFactors
+
+        // absolute offset for the text on the final resulting bitmap
+        val scaledOffset =
+            Offset(
+                x = (memeText.offsetRatioX * templateSize.width) * scaleX,
+                y = (memeText.offsetRatioY * templateSize.height) * scaleY,
+            )
+
+        // text padding of the content padding
+
+        // dp to px
+        val textPaddingPx = TEXT_PADDING_DP * displayDensity
+        val textPaddingBitmapX = textPaddingPx * scaleX
+        val textPaddingBitmapY = textPaddingPx * scaleY
+
+        val scaledFontSize = memeText.fontSize * bitmapScale
+
+        val strokeWidthPx = STROKE_WIDTH_DP * displayDensity * scaleX
+
+        // padding is applied for both ends
+        val paddingDp = TEXT_PADDING_DP * 2
+        val paddingPx = paddingDp * displayDensity
+
+        // dimension in which the text is allowed to expend with
+        val constraintWidth =
+            ((templateSize.width / memeText.scale) * scaleX - paddingPx * scaleX)
+                .roundToInt()
+                .coerceAtLeast(1)
+
+        return ScaledMemeText(
+            text = memeText.text,
+            scaledOffset = scaledOffset,
+            scaledFontSizePx = scaledFontSize.toPx(),
+            strokeWidth = strokeWidthPx,
+            constraintWidth = constraintWidth,
+            textPaddingX = textPaddingBitmapX,
+            textPaddingY = textPaddingBitmapY,
+            rotation = memeText.rotation,
+            scale = memeText.scale,
+            originalText = memeText,
         )
     }
 }
