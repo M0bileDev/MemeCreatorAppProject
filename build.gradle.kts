e18b5843e8ec8d0aaa6a3a7ca46cba8ba4f99873
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-25 00:08:08 +0100

    refactor(gradle): remove hot reload library

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,4 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.serialization) apply false
-    alias(libs.plugins.compose.hot.reload) apply false
 }
