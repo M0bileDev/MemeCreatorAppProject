4e2e53a94ea2e5a8ed59a0b6948837ede30d57eb
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-24 23:24:39 +0100

    refactor(di): update creation process of view model

--- composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/di/ApplicationModule.kt
+++ composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/di/ApplicationModule.kt
@@ -2,13 +2,13 @@ package com.example.memecreatorappproject.di
 
 import com.example.memecreatorappproject.editor.presentation.MemeEditorViewModel
 import org.koin.core.module.Module
-import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 expect val platformModule: Module
 
 val appModule =
     module {
-        viewModel { MemeEditorViewModel(get(), get(), get()) }
+        viewModelOf(::MemeEditorViewModel)
         includes(platformModule)
     }
