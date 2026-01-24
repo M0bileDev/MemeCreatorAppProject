15b4baf177f58a97b5a9fd02957ba810a4be242e
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-24 23:33:28 +0100

    refactor(util): remove suspend contract

--- composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/util/PlatformShareSheet.kt
+++ composeApp/src/commonMain/kotlin/com/example/memecreatorappproject/editor/presentation/util/PlatformShareSheet.kt
@@ -1,5 +1,5 @@
 package com.example.memecreatorappproject.editor.presentation.util
 
 expect class PlatformShareSheet {
-    suspend fun shareFile(filePath: String)
+    fun shareFile(filePath: String)
 }
